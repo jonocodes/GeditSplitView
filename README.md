da2123e63e79dbd0b2063e7cee2ec34d5dd8dd41
Jono <jono@foodnotblogs.com>
2012-02-16 02:08:31 -0500

    First update to get plugin working in Gedit 3

---
+++ README.md
@@ -0,0 +1,5 @@
+
+This is a plugin for showing a split view of a document in Gedit 3. It is a port of Mike Doty's Gedit 2 version.
+
+To install this plugin copy the files into ~/.local/share/gedit/plugins/ and restart Gedit. Then activate the plugin through the preferences.
+
--- SplitView.py
+++ SplitView.py
@@ -5,15 +5,12 @@
 #
 # I'm releasing this under the "The I Really Could Care Less About You Public License."  Read it here:  http://www.revinc.org/pages/license
 
-import gedit
-import gtk
-
-import gobject
 
 import os
 import time
-
 import urllib
+
+from gi.repository import Gtk, Gedit, GObject, Gio
 
 ui_string = """<ui>
   <menubar name="MenuBar">
@@ -31,13 +28,14 @@ class PluginHelper:
         self.window = window
         self.plugin = plugin
 
-        self.ui_id = None
         self.ui_popup_id = None
 
         self.popup_action = None
         self.temp_actions = []
 
         self.document_list = []
+
+        self.ui_id = None
 
         # Add a "toggle split view" item to the View menu
         self.insert_menu_item(window)
@@ -55,19 +53,19 @@ class PluginHelper:
         self.same_document = {}
 
         # I hardly even know how this works, but it gets our encoding.
-        try: self.encoding = gedit.encoding_get_current()
-        except: self.encoding = gedit.gedit_encoding_get_current()
+        try: self.encoding = Gedit.encoding_get_current()
+        except: self.encoding = Gedit.gedit_encoding_get_current()
 
-        self.idle_id = gobject.timeout_add(100, self.monitor_documents)
-
-    def deactivate(self):
+        self.idle_id = GObject.timeout_add(100, self.monitor_documents)
+        
+    def deactivate(self):        
         self.remove_menu_item()
-
-        gobject.source_remove(self.idle_id)
+        
+        GObject.source_remove(self.idle_id)
 
         self.window = None
         self.plugin = None
-
+        
     def update_ui(self):
         return
 
@@ -76,6 +74,7 @@ class PluginHelper:
         self.refresh_popup_submenu()
 
         return True
+
 
     def toggle_split_view(self, unused):
         # Get the current tab...
@@ -115,10 +114,10 @@ class PluginHelper:
                        path = path,
                        name = action_name,
                        action = action_name,
-                       type = gtk.UI_MANAGER_MENU,
+                       type = Gtk.UIManagerItemType.MENU,
                        top = False)
 
-        self.popup_action = gtk.Action(name = action_name, label = "Quick Splitview", tooltip = "Choose another document to view...", stock_id = None)
+        self.popup_action = Gtk.Action(name = action_name, label = "Quick Splitview", tooltip = "Choose another document to view...", stock_id = None)
         self.popup_action.set_visible(True)
 
         self.action_group.add_action(self.popup_action)
@@ -151,16 +150,17 @@ class PluginHelper:
             else:
                 self.tabs_already_using_splitview.append( current_tab )
 
+
         old_other_view = None
         if (current_tab in self.split_views):
             old_other_view = self.split_views[current_tab].get_child2()
 
         # Create a new HPaned or VPaned object for the splitview.
         if (direction == "vertical"):
-            self.split_views[current_tab] = gtk.HPaned()
+            self.split_views[current_tab] = Gtk.HPaned()
 
         else:
-            self.split_views[current_tab] = gtk.VPaned()
+            self.split_views[current_tab] = Gtk.VPaned()
 
         old_view = None
 
@@ -178,65 +178,66 @@ class PluginHelper:
                 each.remove(any)
 
             # Create a scrolled window for the left / top side.
-            sw1 = gtk.ScrolledWindow()
-            sw1.add(old_view)
+            sw1 = Gtk.ScrolledWindow()
+            sw1.add_with_viewport(old_view)
 
             # Set up a new View object
             new_view = None
 
             # If we are viewing a separate file, then just get that document
             if (current_tab in self.alt_views):
-                new_view = gedit.View(self.alt_views[current_tab])#.get_children()[0]
+                new_view = Gedit.View.new(self.alt_views[current_tab])#.get_children()[0]
 
             # Otherwise, just share the same document as the active View.
             # This makes sure changes to either side reflect in the other side.
             else:
-                new_view = gedit.View(current_document)
+                new_view = Gedit.View.new(current_document)
 
             # Second scrolled window.
-            sw2 = gtk.ScrolledWindow()
-            sw2.add(new_view)
+            sw2 = Gtk.ScrolledWindow()
+            sw2.add_with_viewport(new_view)
 
             # Add the two scrolled windows to our Paned object.
             self.split_views[current_tab].add1(sw1)
             self.split_views[current_tab].add2(sw2)
 
             # The next few lines of code just create some buttons.
-            hbox = gtk.HBox()
+            hbox = Gtk.HBox()
 
-            self.btn_cancel = gtk.Button("End Splitview")
+            self.btn_cancel = Gtk.Button("End Splitview")
             self.btn_cancel.connect("clicked", self.end_split_view)
 
-            self.btn_flip = gtk.Button("Vertical Splitview")
+            self.btn_flip = Gtk.Button("Vertical Splitview")
             self.btn_flip.connect("clicked", self.flip_split_view)
 
-            self.btn_view_other_file = gtk.Button("View other file...")
+            self.btn_view_other_file = Gtk.Button("View other file...")
             self.btn_view_other_file.connect("clicked", self.view_other_file)
 
-            self.btn_save_alt_file = gtk.Button("Save Alt Document")
+            self.btn_save_alt_file = Gtk.Button("Save Alt Document")
             self.btn_save_alt_file.connect("clicked", self.save_other_file)
 
-            self.label_other_document = gtk.Label(os.path.basename(current_document.get_uri()))
+            self.label_other_document = Gtk.Label(os.path.basename(current_document.get_uri_for_display()))
             self.label_other_document.set_alignment(1.0, self.label_other_document.get_alignment()[1])
 
-            hbox.pack_start(self.btn_cancel, False, False)
-            hbox.pack_start(self.btn_flip, False, False)
-            hbox.pack_start(self.btn_view_other_file, False, False)
-            hbox.pack_start(self.btn_save_alt_file, False, False)
-            hbox.pack_start(self.label_other_document)
+            hbox.pack_start(self.btn_cancel, False, False, 0)
+            hbox.pack_start(self.btn_flip, False, False, 0)
+            hbox.pack_start(self.btn_view_other_file, False, False, 0)
+            hbox.pack_start(self.btn_save_alt_file, False, False, 0)
+            hbox.pack_start(self.label_other_document, True, True, 0)
 
-            vbox = gtk.VBox()
+            vbox = Gtk.VBox()
 
-            vbox.pack_start(hbox, False, False)
-            vbox.pack_start(self.split_views[current_tab])
+            vbox.pack_start(hbox, False, False, 0)
+            vbox.pack_start(self.split_views[current_tab], True, True, 0)
 
-            each.add_with_viewport(vbox)
+            each.add(vbox)
+#            each.add_with_viewport(vbox)
 
             # The trick of this whole thing is that you have to wait a second for the
             # Paned object to figure out how much room it can take up.  So, we're just
             # going to set a timer that'll check every 500 milliseconds until it
             # decides it can trust the width that the Paned object returns.
-            gobject.timeout_add(500, self.debug)
+            GObject.timeout_add(500, self.debug)
 
         current_tab.show_all()
 
@@ -257,16 +258,16 @@ class PluginHelper:
                 # I put this in a try / except just in case something strange happens.
                 # GEdit just seems to throw the status bar in an element and add 5
                 # children or something like that into it, so I just loop through
-                # until I find a gtk.Label that starts with "  Ln".  Seems hacky?
+                # until I find a Gtk.Label that starts with "  Ln".  Seems hacky?
                 # Yeah.  Go make your own splitview if ya don't like it.  :P
                 try:
                     for each in self.window.get_statusbar().get_children():
-                        if (type(each) == gtk.Statusbar):
+                        if (type(each) == Gtk.Statusbar):
                             frame = each.get_children()[0]
 
                             frame_child = frame.get_children()[0]
 
-                            if (type(frame_child) == gtk.Label):
+                            if (type(frame_child) == Gtk.Label):
                                 if (frame_child.get_label().lstrip().startswith("Ln ")):
                                     frame_child.set_label("  Ln %d, Col %d [2]" % (line, column))
 
@@ -306,11 +307,12 @@ class PluginHelper:
 
             if (current_tab in self.alt_views):
                 self.alt_views.pop(current_tab)
-
+            
             if (current_tab in self.tabs_already_using_splitview):
                 index = self.tabs_already_using_splitview.index(current_tab)
 
                 self.tabs_already_using_splitview.pop(index)
+
 
     # Basically recreate the split view.
     def flip_split_view(self, button):
@@ -327,19 +329,18 @@ class PluginHelper:
             self.btn_flip.set_label("Vertical Splitview")
 
         current_tab = self.window.get_active_tab()
-
         if (current_tab in self.alt_views):
-            self.label_other_document.set_label(os.path.basename(self.alt_views[current_tab].get_uri().replace("%20", " ")))
+            self.label_other_document.set_label(os.path.basename(self.alt_views[current_tab].get_uri_for_display().replace("%20", " ")))
 
     # Create a dialog that lets the user select a different document to view...
     def view_other_file(self, button):
-        dialog = gtk.FileChooserDialog("Open...", None,
-                                       gtk.FILE_CHOOSER_ACTION_OPEN,
-                                       (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-                                        gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        dialog = Gtk.FileChooserDialog("Open...", None,
+                                       Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
 
         current_document = self.window.get_active_document()
-        current_path = urllib.unquote( current_document.get_uri().replace("file://", "") )
+        current_path = urllib.unquote( current_document.get_uri_for_display().replace("file://", "") )
         dialog.set_current_folder( os.path.dirname(current_path) )
 
         filter_list = [
@@ -348,7 +349,7 @@ class PluginHelper:
         ]
 
         for dict in filter_list:
-            filter = gtk.FileFilter()
+            filter = Gtk.FileFilter()
             filter.set_name(dict["label"])
             for each in dict["patterns"]:
                 filter.add_pattern(each)
@@ -360,7 +361,7 @@ class PluginHelper:
 
         response = dialog.run()
 
-        if (response == gtk.RESPONSE_OK):
+        if (response == Gtk.ResponseType.OK):
             self.load_other_file(dialog.get_filename())
 
         else:
@@ -376,15 +377,18 @@ class PluginHelper:
 
         self.split_views[current_tab].remove(self.split_views[current_tab].get_children()[1])
 
-        new_document = gedit.Document()#.gedit_document_new()
+        new_document = Gedit.Document()#.gedit_document_new()
 
-        if (raw_filename):
-            new_document.load("file://" + source.replace(" ", "%20"), self.encoding, 1, True)
+        filepath = source.replace(" ", "%20")
 
-        else:
-            new_document.load(source, self.encoding, 1, True)
+        if not raw_filename:
+            filepath = "file://" + str(filepath)
 
-        new_view = gedit.View(new_document)#.gedit_view_new(new_document)
+        gfile = Gio.File.new_for_path(filepath)
+
+        new_document.load(gfile, self.encoding, 1, 1, True)
+
+        new_view = Gedit.View.new(new_document)#.gedit_view_new(new_document)
 
         new_document.connect("mark-set", self.update_line_column_data)
 
@@ -392,7 +396,7 @@ class PluginHelper:
 
         self.alt_views[current_tab] = new_document
 
-        sw = gtk.ScrolledWindow()
+        sw = Gtk.ScrolledWindow()
         sw.add(new_view)
 
         self.split_views[current_tab].add2(sw)
@@ -411,16 +415,16 @@ class PluginHelper:
     def show_save2_dialog(self, button):
         current_tab = self.window.get_active_tab()
 
-        dialog = gtk.Dialog(title = "Help me help you!",
+        dialog = Gtk.Dialog(title = "Help me help you!",
                             parent = None,
-                            flags = gtk.DIALOG_MODAL,
-                            buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-                                       gtk.STOCK_NO, gtk.RESPONSE_NO,
-                                       gtk.STOCK_YES, gtk.RESPONSE_YES)
+                            flags = Gtk.DialogFlags.MODAL,
+                            buttons = (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                       Gtk.STOCK_NO, Gtk.ResponseType.NO,
+                                       Gtk.STOCK_YES, Gtk.ResponseType.YES)
                            )
 
-        hbox = gtk.HBox()
-        hbox.pack_start(gtk.Label("Save changes to '%s'?" % os.path.basename(self.alt_views[current_tab].get_uri().replace("%20", " "))))
+        hbox = Gtk.HBox()
+        hbox.pack_start(Gtk.Label("Save changes to '%s'?" % os.path.basename(self.alt_views[current_tab].get_uri_for_display().replace("%20", " "))))
 
         dialog.vbox.pack_start(hbox)
 
@@ -428,12 +432,12 @@ class PluginHelper:
 
         response = dialog.run()
 
-        if (response == gtk.RESPONSE_YES):
+        if (response == Gtk.ResponseType.YES):
             self.alt_views[current_tab].save(0)
 
             self.show_confirm_save_dialog(None)
 
-        elif (response == gtk.RESPONSE_CANCEL):
+        elif (response == Gtk.ResponseType.CANCEL):
             dialog.destroy()
             return False
 
@@ -447,20 +451,20 @@ class PluginHelper:
     def show_confirm_save_dialog(self, button):
         current_tab = self.window.get_active_tab()
 
-        dialog = gtk.Dialog(title = "Document saved!",
+        dialog = Gtk.Dialog(title = "Document saved!",
                             parent = None,
-                            flags = gtk.DIALOG_MODAL,
-                            buttons = (gtk.STOCK_OK, gtk.RESPONSE_OK,
-                                       gtk.STOCK_YES, gtk.RESPONSE_YES)
+                            flags = Gtk.DialogFlags.MODAL,
+                            buttons = (Gtk.STOCK_OK, Gtk.ResponseType.OK,
+                                       Gtk.STOCK_YES, Gtk.ResponseType.YES)
                            )
 
-        hbox = gtk.HBox()
-        hbox.pack_start(gtk.Label("You have successfully saved"))
+        hbox = Gtk.HBox()
+        hbox.pack_start(Gtk.Label("You have successfully saved"))
 
         dialog.vbox.pack_start(hbox)
 
-        hbox = gtk.HBox()
-        hbox.pack_start(gtk.Label("'%s'!" % os.path.basename(self.alt_views[current_tab].get_uri().replace("%20", " "))))
+        hbox = Gtk.HBox()
+        hbox.pack_start(Gtk.Label("'%s'!" % os.path.basename(self.alt_views[current_tab].get_uri_for_display().replace("%20", " "))))
 
         dialog.vbox.pack_start(hbox)
 
@@ -468,7 +472,7 @@ class PluginHelper:
 
         response = dialog.run()
 
-        if (response == gtk.RESPONSE_OK):
+        if (response == Gtk.ResponseType.OK):
             pass
 
         else:
@@ -477,19 +481,19 @@ class PluginHelper:
         dialog.destroy()
 
     def show_error_dialog(self):
-        dialog = gtk.Dialog(title = "Error",
+        dialog = Gtk.Dialog(title = "Error",
                             parent = None,
-                            flags = gtk.DIALOG_MODAL,
-                            buttons = (gtk.STOCK_OK, gtk.RESPONSE_OK)
+                            flags = Gtk.DialogFlags.MODAL,
+                            buttons = (Gtk.STOCK_OK, Gtk.RESPONSE_OK)
                            )
 
-        hbox = gtk.HBox()
-        hbox.pack_start(gtk.Label("You can't splitview"))
+        hbox = Gtk.HBox()
+        hbox.pack_start(Gtk.Label("You can't splitview"))
 
         dialog.vbox.pack_start(hbox)
 
-        hbox = gtk.HBox()
-        hbox.pack_start(gtk.Label("an empty document."))
+        hbox = Gtk.HBox()
+        hbox.pack_start(Gtk.Label("an empty document."))
 
         dialog.vbox.pack_start(hbox)
 
@@ -517,14 +521,14 @@ class PluginHelper:
 
     def insert_menu_item(self, window):
         manager = self.window.get_ui_manager()
-
-        self.action_group = gtk.ActionGroup("PluginActions")
-
+        
+        self.action_group = Gtk.ActionGroup("PluginActions")
+        
         # Create an action for the "Run in python" menu option
         # and set it to call the "run_document_in_python" function.
-        self.split_view_action = gtk.Action(name="ExamplePy", label="Toggle Split View", tooltip="Create a split view of the current document", stock_id=gtk.STOCK_REFRESH)
+        self.split_view_action = Gtk.Action(name="ExamplePy", label="Toggle Split View", tooltip="Create a split view of the current document", stock_id=Gtk.STOCK_REFRESH)
         self.split_view_action.connect("activate", self.toggle_split_view)
-
+        
         # Add the action with Ctrl + F5 as its keyboard shortcut.
         self.action_group.add_action_with_accel(self.split_view_action, "<Ctrl><Shift>T")
 
@@ -534,9 +538,11 @@ class PluginHelper:
         # Add the item to the "Views" menu.
         self.ui_id = manager.add_ui_from_string(ui_string)
 
+        manager.ensure_update()
+        
     def remove_menu_item(self):
-        self.destroy_popup_menu()
-
+        panel = self.window.get_side_panel()
+        
         manager = self.window.get_ui_manager()
 
         manager.remove_ui(self.ui_id)
@@ -578,10 +584,10 @@ class PluginHelper:
                                path = path,
                                name = action_name,
                                action = action_name,
-                               type = gtk.UI_MANAGER_MENUITEM,
+                               type = Gtk.UIManagerItemType.MENU,
                                top = False)
 
-                action = gtk.Action(name = action_name, label = doc_list[i].get_short_name_for_display(), tooltip = "None!", stock_id = None)
+                action = Gtk.Action(name = action_name, label = doc_list[i].get_short_name_for_display(), tooltip = "None!", stock_id = None)
                 action.connect("activate", lambda a, doc = doc_list[i]: self.quick_splitview(doc))
                 action.set_visible(True)
 
@@ -610,16 +616,22 @@ class PluginHelper:
         # "worked" or not.
         self.load_other_file(doc.get_uri(), raw_filename = False)
 
-class SplitViewPlugin(gedit.Plugin):
+
+class WindowActivatable(GObject.Object, Gedit.WindowActivatable):
+
+    window = GObject.property(type=Gedit.Window)
+
     def __init__(self):
-        gedit.Plugin.__init__(self)
+        GObject.Object.__init__(self)
         self.instances = {}
-
-    def activate(self, window):
-        self.instances[window] = PluginHelper(self, window)
-
-    def deactivate(self, window):
-        self.instances[window].deactivate()
-
-    def update_ui(self, window):
-        self.instances[window].update_ui()
+        
+    def do_activate(self):
+        self.instances[self.window] = PluginHelper(self, self.window)
+        
+    def do_deactivate(self):
+        if self.window in self.instances:
+            self.instances[self.window].deactivate()
+        
+    def update_ui(self):
+        if self.window in self.instances:
+            self.instances[self.window].update_ui()
